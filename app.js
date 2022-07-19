bf68514416ed6ccd3780877eaf0c2e0b12bb1000
Toni Deplano <montana.toni@gmail.com>
2022-07-19 12:02:57 +0200

    server et routet

--- app.js
+++ app.js
@@ -7,7 +7,25 @@
 require('dotenv-flow').config();
 // console.log(process.env);
 
-const {MESSAGE, NODE_ENV} = process.env;
+const {MESSAGE, NODE_ENV, PORT} = process.env;
 console.log('Lancé en ', NODE_ENV, ' : ', MESSAGE);
+const express = require('express');
+const app = express();
+
+
+
+app.get('/Users', (req, res) => {
+    const data  = {
+        msg : 'Coucou'
+    }
+    res.json(data);
+})
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server up on port : ${PORT} [${NODE_ENV}]`);
+})
 
 
